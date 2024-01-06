65d4f32ea1859a2ef417f94fed01ca9fd0c27d53
LazarevSlava <549132w@gmail.com>
2024-01-06 15:59:38 +0200

    correct ref

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
                     © {new Date().getFullYear()} Copyright Text
                     <a
                         className="grey-text text-lighten-4 right"
-                        href="https://lazarevslava.github.io/react-shop/"
+                        href="https://github.com/LazarevSlava/react-shop/"
                         target="_blank"
                         rel="noreferrer"
                     >
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ function Header() {
                 <ul id="nav-mobile" className="right hide-on-med-and-down">
                     <li>
                         <a
-                            href="https://lazarevslava.github.io/react-shop/"
+                            href="https://github.com/LazarevSlava/react-shop"
                             target="_blank"
                             rel="noreferrer"
                         >
