ca765d43d4f61f0c609ac210356b7d53e52248e1
LazarevSlava <549132w@gmail.com>
2024-01-07 15:33:28 +0200

    reducer

--- src/components/BasketList.jsx
+++ src/components/BasketList.jsx
@@ -1,13 +1,10 @@
+import { useContext } from 'react';
+import { ShopContext } from './context';
 import { BasketItem } from './Basketitem';
 
 function BasketList(props) {
-    const {
-        order = [],
-        handleBasketShow = Function.prototype,
-        removeFromBasket = Function.prototype,
-        incQuantity = Function.prototype,
-        decQuantity = Function.prototype,
-    } = props;
+    const { order = [], handleBasketShow = Function.prototype } =
+        useContext(ShopContext);
 
     const totalPrice = order.reduce((sum, el) => {
         return sum + el.price * el.quantity;
@@ -17,15 +14,7 @@ function BasketList(props) {
         <div className="collection basket-list">
             <ul className="collection-item active">Корзина</ul>
             {order.length ? (
-                order.map((item) => (
-                    <BasketItem
-                        key={item.id}
-                        {...item}
-                        removeFromBasket={removeFromBasket}
-                        incQuantity={incQuantity}
-                        decQuantity={decQuantity}
-                    />
-                ))
+                order.map((item) => <BasketItem key={item.id} {...item} />)
             ) : (
                 <ul className="collection-item">Корзина пуста</ul>
             )}
@@ -35,9 +24,14 @@ function BasketList(props) {
                     Оформить
                 </button>
             </ul>
-            <span onClick={handleBasketShow} className="basket-close">
-                <i className="material-icons">close</i>
-            </span>
+            <li>
+                <button
+                    onClick={handleBasketShow}
+                    className="material-icons basket-close"
+                >
+                    close
+                </button>
+            </li>
         </div>
     );
 }
